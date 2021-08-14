9d790c826ce187cbd48033b0803cde526257edb9
Divyansh Pandey <78445453+inevitableDivu@users.noreply.github.com>
2021-08-14 13:43:45 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://divu:steve3007S@cluster0.z6gqx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    mongoURI: "",
     secretOrKey: "divYanshPanDey30071995@@mernAuth"
 };
